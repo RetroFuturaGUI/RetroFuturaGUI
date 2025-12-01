d30a84dc2959f4d717533e9e26e397c74ac27a78
Lawn Meower <CosmoCortney@users.noreply.github.com>
2025-12-02 00:10:52 +0100

    Added Button widget class

---
+++ source/widgets/Button.cpp
@@ -0,0 +1,210 @@
+#include "Button.hpp"
+
+RetroFuturaGUI::Button::Button(const IdentityParams &identity, const GeometryParams2D &geometry, const TextParams &textParams)
+    : IWidget(identity, geometry)
+{
+    _rectangle = std::make_unique<Rectangle>(const_cast<Projection&>(geometry._Projection), _backgroundColorEnabled, geometry._Size.x, geometry._Size.y, geometry._Position.x, geometry._Position.y, geometry._Rotation);
+    _lineBorder = std::make_unique<LineFrame>(const_cast<Projection&>(geometry._Projection), _borderColorEnabled, geometry._Size.x, geometry._Size.y, geometry._Position.x, geometry._Position.y, 5.0f, geometry._Rotation);
+    _text = std::make_unique<Text>(geometry, textParams);
+}
+
+void RetroFuturaGUI::Button::Draw()
+{
+    interact();
+    _rectangle->Draw();
+    _lineBorder->Draw();
+    _text->Draw();
+}
+
+void RetroFuturaGUI::Button::Connect_OnClick(const typename Signal<>::Slot &slot, const bool async)
+{
+    if (async)
+        _onClick.Connect(slot);
+    else
+        _onClickAsync.Connect(slot); 
+}
+
+void RetroFuturaGUI::Button::Connect_OnRelease(const typename Signal<>::Slot &slot, const bool async)
+{
+    if (async)
+        _onRelease.Connect(slot);
+    else
+        _onReleaseAsync.Connect(slot); 
+}
+
+void RetroFuturaGUI::Button::Connect_WhileHover(const typename Signal<>::Slot &slot, const bool async)
+{
+    if (async)
+        _whileHover.Connect(slot);
+    else
+        _whileHoverAsync.Connect(slot); 
+}
+
+void RetroFuturaGUI::Button::Connect_OnMouseEnter(const typename Signal<>::Slot &slot, const bool async)
+{
+    if (async)
+        _onMouseEnter.Connect(slot);
+    else
+        _onMouseEnterAsync.Connect(slot); 
+}
+
+void RetroFuturaGUI::Button::Connect_OnMouseLeave(const typename Signal<>::Slot &slot, const bool async)
+{
+    if (async)
+        _onMouseLeave.Connect(slot);
+    else
+        _onMouseLeaveAsync.Connect(slot); 
+}
+
+void RetroFuturaGUI::Button::Connect_OnEnable(const typename Signal<>::Slot & slot, const bool async)
+{
+    if (async)
+        _onEnable.Connect(slot);
+    else
+        _onEnableAsync.Connect(slot); 
+}
+
+void RetroFuturaGUI::Button::Connect_OnDisable(const typename Signal<>::Slot &slot, const bool async)
+{
+    if (async)
+        _onDisable.Connect(slot);
+    else
+        _onDisableAsync.Connect(slot); 
+}
+
+void RetroFuturaGUI::Button::SetEnabled(const bool enable)
+{
+    _isEnabledFlag = enable;
+
+    if(_isEnabledFlag)
+    {
+        _onEnableAsync.EmitAsync();
+        _onEnable.Emit();
+        SetBackgroundColor(_backgroundColorEnabled);
+        return;
+    }
+
+    _onDisableAsync.EmitAsync();
+    _onDisable.Emit();
+    SetBackgroundColor(_backgroundColorDisabled);
+}
+
+bool RetroFuturaGUI::Button::IsEnabled() const
+{
+    return _isEnabledFlag;
+}
+
+void RetroFuturaGUI::Button::SetBackgroundColor(const glm::vec4 &color)
+{
+    _rectangle->SetColor(color);
+}
+
+glm::vec4 RetroFuturaGUI::Button::GetBackgroundColor() const
+{
+    return _rectangle->GetColor();
+}
+
+void RetroFuturaGUI::Button::SetTextColor(const glm::vec4 &color)
+{
+    _text->SetColor(color);
+}
+
+glm::vec4 RetroFuturaGUI::Button::GetTextColor() const
+{
+    return _text->GetColor();
+}
+
+void RetroFuturaGUI::Button::SetLineBorderColor(const glm::vec4 &color)
+{
+    _lineBorder->SetColor(color);
+}
+
+glm::vec4 RetroFuturaGUI::Button::GetLineBorderColor() const
+{
+    return _lineBorder->GetColor();
+}
+
+void RetroFuturaGUI::Button::interact()
+{
+    auto mousePos = InputManager::GetMousePosition();
+    bool isMouseButtonPressed = InputManager::IsMouseButtonPressed(GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS;
+    bool isMouseInside = isPointInside(glm::vec2(mousePos));
+
+    if(!_isEnabledFlag || !isMouseInside) //no action and mouse leave
+    {
+        if(_mouseEnteredFlag)
+        {
+            _mouseEnteredFlag = false;
+            _onMouseLeaveAsync.EmitAsync();
+            _onMouseLeave.Emit();
+            setColors(ColorSetKeys::Enabled);
+        }
+
+        return;
+    }
+    
+    bool isHovering = _isEnabledFlag && isMouseInside;
+    if(isHovering) // hover
+    {
+        _whileHoverAsync.EmitAsync();
+        _whileHover.Emit();
+    }
+
+    if (isHovering && !_mouseLeftFlag && !_mouseEnteredFlag) //enter
+    {
+        _mouseEnteredFlag = true;
+        _onMouseEnterAsync.EmitAsync();
+        _onMouseEnter.Emit();
+        setColors(ColorSetKeys::Hover);
+    }
+
+    if (isMouseButtonPressed && !_wasClicked) //click
+    {
+        _onClickAsync.EmitAsync();
+        _onClick.Emit();
+        setColors(ColorSetKeys::Clicked);
+    }
+    else if(!isMouseButtonPressed && _wasClicked) //release
+    {
+        _onReleaseAsync.EmitAsync();
+        _onRelease.Emit();
+
+        if(isHovering)
+            setColors(ColorSetKeys::Hover);
+        else
+            setColors(ColorSetKeys::Enabled);
+    }
+
+    _wasClicked = isMouseButtonPressed;
+}
+
+void RetroFuturaGUI::Button::setColors(const ColorSetKeys key)
+{
+    switch(key)
+    {
+        case ColorSetKeys::Enabled:
+        {
+            SetBackgroundColor(_backgroundColorEnabled);
+            SetLineBorderColor(_borderColorEnabled);
+            SetTextColor(_textColorEnabled);
+        } break;
+        case ColorSetKeys::Clicked:
+        {
+            SetBackgroundColor(_backgroundColorClicked);
+            SetLineBorderColor(_borderColorClicked);
+            SetTextColor(_textColorClicked);
+        } break;
+        case ColorSetKeys::Hover:
+        {
+            SetBackgroundColor(_backgroundColorHover);
+            SetLineBorderColor(_borderColorHover);
+            SetTextColor(_textColorHover);
+        } break;
+        default: //Disabled
+        {
+            SetBackgroundColor(_backgroundColorDisabled);
+            SetLineBorderColor(_borderColorDisabled);
+            SetTextColor(_textColorDisabled);
+        }
+    }
+}
---
+++ source/widgets/Button.hpp
@@ -0,0 +1,85 @@
+#pragma once
+#include "IncludeHelper.hpp"
+#include "InputManager.hpp"
+#include "LineFrame.hpp"
+#include "Rectangle.hpp"
+#include "Signal.hpp"
+#include "Text.hpp"
+#include "IWidget.hpp"
+
+namespace RetroFuturaGUI
+{
+    class Button : public IWidget
+    {
+    public:
+        Button(const IdentityParams& identity, const GeometryParams2D& geometry, const TextParams& textParams);
+        void Draw();
+        void Connect_OnClick(const typename Signal<>::Slot& slot, const bool async);
+        void Connect_OnRelease(const typename Signal<>::Slot& slot, const bool async);
+        void Connect_WhileHover(const typename Signal<>::Slot& slot, const bool async);
+        void Connect_OnMouseEnter(const typename Signal<>::Slot& slot, const bool async);
+        void Connect_OnMouseLeave(const typename Signal<>::Slot& slot, const bool async);
+        void Connect_OnEnable(const typename Signal<>::Slot& slot, const bool async);
+        void Connect_OnDisable(const typename Signal<>::Slot& slot, const bool async);
+        void SetEnabled(const bool enable);
+        bool IsEnabled() const;
+        void SetBackgroundColor(const glm::vec4& color);
+        glm::vec4 GetBackgroundColor() const;
+        void SetTextColor(const glm::vec4& color);
+        glm::vec4 GetTextColor() const;
+        void SetLineBorderColor(const glm::vec4& color);
+        glm::vec4 GetLineBorderColor() const;
+
+    private:
+        //elements
+        std::unique_ptr<Rectangle> _rectangle;
+        std::unique_ptr<LineFrame> _lineBorder;
+        std::unique_ptr<Text> _text;
+        
+        //logic
+        bool _isEnabledFlag = true;
+        bool _wasClicked = false;
+        bool _colorSetFlag = false;
+        bool _mouseEnteredFlag = false;
+        bool _mouseLeftFlag = false;
+        Signal<> _onClick;
+        Signal<> _onClickAsync;
+        Signal<> _onRelease;
+        Signal<> _onReleaseAsync;
+        Signal<> _whileHover;
+        Signal<> _whileHoverAsync;
+        Signal<> _onMouseEnter;
+        Signal<> _onMouseEnterAsync;
+        Signal<> _onMouseLeave;
+        Signal<> _onMouseLeaveAsync;
+        Signal<> _onEnable;
+        Signal<> _onEnableAsync;
+        Signal<> _onDisable;
+        Signal<> _onDisableAsync;
+
+        //style
+        enum class ColorSetKeys : u32 
+        {
+            Disabled,
+            Enabled,
+            Clicked,
+            Hover
+        };
+
+        glm::vec4 _backgroundColorEnabled = glm::vec4(0.5f, 0.5f, 0.5f, 1.0f);
+        glm::vec4 _backgroundColorDisabled = glm::vec4(0.25f, 0.25f, 0.25f, 1.0f);
+        glm::vec4 _backgroundColorClicked = glm::vec4(0.6f, 0.6f,0.6f, 1.0f);
+        glm::vec4 _backgroundColorHover = glm::vec4(0.55f, 0.55f, 0.55f, 1.0f);
+        glm::vec4 _borderColorEnabled = glm::vec4(0.25f, 0.25f, 0.25f, 1.0f);
+        glm::vec4 _borderColorDisabled = glm::vec4(0.2f, 0.2f, 0.2f, 1.0f);
+        glm::vec4 _borderColorClicked = glm::vec4(0.3f, 0.3f, 1.3f, 1.0f);
+        glm::vec4 _borderColorHover = glm::vec4(0.275f, 0.275f, 0.275f, 1.0f);
+        glm::vec4 _textColorEnabled = glm::vec4(1.0f);
+        glm::vec4 _textColorDisabled = glm::vec4(0.5f, 0.5f,0.5f, 1.0f);
+        glm::vec4 _textColorClicked = glm::vec4(1.0f);
+        glm::vec4 _textColorHover = glm::vec4(1.0f, 0.0f, 1.0f, 1.0f);
+
+        void interact();
+        void setColors(const ColorSetKeys key);
+    };
+}
