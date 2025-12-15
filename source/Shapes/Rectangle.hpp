e3a04e0ea535b72f17507e5be25e67776c097135
Lawn Meower <CosmoCortney@users.noreply.github.com>
2025-12-16 00:27:29 +0100

    Rectangle: refactoring

--- source/Shapes/Rectangle.hpp
+++ source/Shapes/Rectangle.hpp
@@ -33,6 +33,7 @@ namespace RetroFuturaGUI
         glm::vec4 GetColor() const;
 
     private:
+        //Geometry
         f32 _vertices[3 * 4] = 
         {
             -0.5f, -0.5f, 0.0f,
@@ -46,25 +47,32 @@ namespace RetroFuturaGUI
             0, 1, 2,
             2, 3, 0
         };
+        
+        u32 
+            _vao { 0 },
+            _vbo { 0 },
+            _ebo { 0 };
 
         Projection& _projection;
-        glm::mat4 _scalingMatrix = glm::mat4(1.0f);
-        glm::mat4 _translationMatrix = glm::mat4(1.0f);
-        glm::mat4 _rotationMatrix = glm::mat4(1.0f);
-        std::unique_ptr<glm::vec4[]> _colors;
-        FillType _fillType = FillType::SOLID;
-        u32 _colorCount = 0;
-        f32 _gradientOffset = 0.0f;
-        f32 _animationSpeed = 0.0003f;
-        f32 _gradientDegree = 45.0f;
-        f32 _gradientRotationSpeed = 0.02f;
-        u32 _vao = 0;
-        u32 _vbo = 0;
-        u32 _ebo = 0;
-        glm::vec2 _scale = glm::vec2(0.01f, 0.01f);
-        glm::vec2 _position = glm::vec2(0.0f, 0.0f);
-        f32 _rotation = 0.0f;
+        glm::mat4 
+            _scalingMatrix { 1.0f },
+            _translationMatrix { 1.0f },
+            _rotationMatrix { 1.0f };
+        glm::vec2
+            _scale { 0.01f },
+            _position { 0.0f };
+        f32 _rotation { 0.0f };
         glm::vec4 _vertexPosition = glm::vec4(0.0f, 0.0f, 0.0f, 1.0f);
+
+        // Fill
+        std::unique_ptr<glm::vec4[]> _colors;
+        FillType _fillType { FillType::SOLID };
+        u32 _colorCount { 0 };
+        f32
+            _gradientOffset { 0.0f },
+            _animationSpeed { 0.0003f },
+            _gradientDegree { 45.0f },
+            _gradientRotationSpeed { 0.02f };
 
         void setupMesh();
         void initBasic(std::span<const glm::vec4> colors);
