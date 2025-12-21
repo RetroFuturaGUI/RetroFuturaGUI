2d5c6bfbe5750c60258e3392177b050a15ad9fc8
Lawn Meower <CosmoCortney@users.noreply.github.com>
2025-12-21 19:43:47 +0100

    Fill Shader: Rounded Corners feature

--- source/Shaders/ShaderSource/Fill.fs
+++ source/Shaders/ShaderSource/Fill.fs
@@ -1,15 +1,45 @@
 #version 330 core
+#define ROUNDED_CORNERS 1
 
 layout(location = 0) out vec4 Color;
 uniform vec4 uColor;
+uniform vec4 uCornerRadii;
+uniform vec2 uScale;
+uniform int uDIP;
+
+in vec2 vLocalPos;
 
 void main()
 {
+    if((uDIP & ROUNDED_CORNERS) != 0)
+    {
+        vec2 scaledPos = vLocalPos * uScale;
+        vec2 halfSize = vec2(0.5) * uScale;
+
+        float cornerRadius;
+        if (scaledPos.x >= 0.0 && scaledPos.y >= 0.0)
+        {
+            cornerRadius = uCornerRadii.y;
+        }
+        else if (scaledPos.x < 0.0 && scaledPos.y >= 0.0)
+        {
+            cornerRadius = uCornerRadii.x;
+        }
+        else if (scaledPos.x < 0.0 && scaledPos.y < 0.0)
+        {
+            cornerRadius = uCornerRadii.w;
+        }
+        else
+        {
+            cornerRadius = uCornerRadii.z;
+        }
+        
+        vec2 p = abs(scaledPos) - halfSize + vec2(cornerRadius);
+        float dist = length(max(p, 0.0)) + min(max(p.x, p.y), 0.0) - cornerRadius;
+
+        if (dist > 0.0)
+            discard;
+    }
+
     Color = uColor;
 }
-
-/*  Usage: 
-    i32 locColor = glGetUniformLocation(_shader->GetProgramId(), "uColors");
-    glm::vec4 color = {0.5f, 0.0f, 1.0f, 1.0f};
-    glUniform4fv(locColor, 1, &color[0]);
-*/
--- source/Shaders/ShaderSource/Fill.vs
+++ source/Shaders/ShaderSource/Fill.vs
@@ -6,7 +6,10 @@ uniform mat4 uPosition;
 uniform mat4 uRotation;
 layout(location = 0) in vec4 Position;
 
+out vec2 vLocalPos;
+
 void main()
 {
+    vLocalPos = Position.xy;
     gl_Position = uProjection * uPosition * uRotation * uScaling * Position;
 }
