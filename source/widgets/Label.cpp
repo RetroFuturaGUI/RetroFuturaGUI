584f5930425a31eb9f256589a1ee1e1e85833c60
Lawn Meower <CosmoCortney@users.noreply.github.com>
2026-01-02 23:39:49 +0100

    Label: set type ID

--- source/widgets/Label.cpp
+++ source/widgets/Label.cpp
@@ -3,6 +3,7 @@
 RetroFuturaGUI::Label::Label(const IdentityParams& identity, const GeometryParams2D& geometry, const TextParams& textParams)
 : IWidget(identity, geometry)
 {
+    _widgetTypeID = WidgetTypeID::Label;
     _textObject = std::make_unique<Text>(geometry, textParams);
 
 
