430f4255bfd3a41aefb998f9e9b91bf48774ea4d
Lawn Meower <CosmoCortney@users.noreply.github.com>
2026-08-06 01:53:38 +0200

    Texture: Moved all OpenGL-related stuff here for a more module implementation

--- source/OpenGlHelpers/Texture.cpp
+++ source/OpenGlHelpers/Texture.cpp
@@ -1,13 +1,150 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 #include "Texture.hpp"
+#include "ShaderManager.hpp"
 #include <print>
 
-RetroFuturaGUI::Texture::Texture(std::string_view path, const bool flipVertically)
-: _verticallyFlipped(flipVertically)
+RetroFuturaGUI::Texture::Texture(std::string_view path, const bool flipVertically, Projection* projection)
+    : _verticallyFlipped(flipVertically), _projection(projection)
 {
     loadTexture(path);
     _aspectRatio = static_cast<f32>(_resolution.x) / static_cast<f32>(_resolution.y);
+    uploadToGPU();
+
+    if (_projection)
+        setupQuad();
+}
+
+RetroFuturaGUI::Texture::Texture(Texture&& other) noexcept
+    : _id(other._id), _verticallyFlipped(other._verticallyFlipped), _aspectRatio(other._aspectRatio),
+      _resolution(other._resolution), _colorChannelCount(other._colorChannelCount),
+      _texture(std::move(other._texture)), _path(std::move(other._path)), _type(std::move(other._type)),
+      _vao(other._vao), _vbo(other._vbo), _ebo(other._ebo), _projection(other._projection),
+      _scalingMatrix(other._scalingMatrix), _translationMatrix(other._translationMatrix), _rotationMatrix(other._rotationMatrix)
+{
+    other._id = 0;
+    other._vao = 0;
+    other._vbo = 0;
+    other._ebo = 0;
+}
+
+RetroFuturaGUI::Texture& RetroFuturaGUI::Texture::operator=(Texture&& other) noexcept
+{
+    if (this == &other)
+        return *this;
+
+    glDeleteTextures(1, &_id);
+    glDeleteVertexArrays(1, &_vao);
+    glDeleteBuffers(1, &_vbo);
+    glDeleteBuffers(1, &_ebo);
+
+    _id = other._id;
+    _verticallyFlipped = other._verticallyFlipped;
+    _aspectRatio = other._aspectRatio;
+    _resolution = other._resolution;
+    _colorChannelCount = other._colorChannelCount;
+    _texture = std::move(other._texture);
+    _path = std::move(other._path);
+    _type = std::move(other._type);
+    _vao = other._vao;
+    _vbo = other._vbo;
+    _ebo = other._ebo;
+    _projection = other._projection;
+    _scalingMatrix = other._scalingMatrix;
+    _translationMatrix = other._translationMatrix;
+    _rotationMatrix = other._rotationMatrix;
+
+    other._id = 0;
+    other._vao = 0;
+    other._vbo = 0;
+    other._ebo = 0;
+
+    return *this;
+}
+
+RetroFuturaGUI::Texture::~Texture()
+{
+    glDeleteTextures(1, &_id);
+    glDeleteVertexArrays(1, &_vao);
+    glDeleteBuffers(1, &_vbo);
+    glDeleteBuffers(1, &_ebo);
+}
+
+void RetroFuturaGUI::Texture::Draw() const
+{
+    if (_projection)
+    {
+        ShaderManager::GetTextureFillShader().UseProgram();
+        ShaderManager::GetTextureFillShader().SetUniformInt("uTexture", 0);
+        ShaderManager::GetTextureFillShader().SetUniformMat4("uProjection", _projection->GetProjectionMatrix());
+        ShaderManager::GetTextureFillShader().SetUniformMat4("uPosition", _translationMatrix);
+        ShaderManager::GetTextureFillShader().SetUniformMat4("uScaling", _scalingMatrix);
+        ShaderManager::GetTextureFillShader().SetUniformMat4("uRotation", _rotationMatrix);
+    }
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, _id);
+
+    if (_projection)
+    {
+        glBindVertexArray(_vao);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+        glBindVertexArray(0);
+    }
+}
+
+void RetroFuturaGUI::Texture::SetSize(const glm::vec2& size)
+{
+    _scalingMatrix = glm::scale(glm::mat4(1.0f), glm::vec3(size, 1.0f));
+}
+
+void RetroFuturaGUI::Texture::SetPosition(const glm::vec3& position)
+{
+    _translationMatrix = glm::translate(glm::mat4(1.0f), position);
+}
+
+void RetroFuturaGUI::Texture::SetRotation(const f32 rotation)
+{
+    _rotationMatrix = glm::rotate(glm::mat4(1.0f), glm::radians(rotation), glm::vec3(0.0f, 0.0f, 1.0f));
+}
+
+void RetroFuturaGUI::Texture::setupQuad()
+{
+    glGenVertexArrays(1, &_vao);
+    glBindVertexArray(_vao);
+
+    glGenBuffers(1, &_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, _vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(_vertices), _vertices, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(f32), nullptr);
+
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(f32), (void*)(3 * sizeof(f32)));
+
+    glGenBuffers(1, &_ebo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(_indices), _indices, GL_STATIC_DRAW);
+
+    glBindVertexArray(0);
+}
+
+void RetroFuturaGUI::Texture::uploadToGPU()
+{
+    if (_texture.empty())
+        return;
+
+    glGenTextures(1, &_id);
+    glBindTexture(GL_TEXTURE_2D, _id);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    u32 format = (_colorChannelCount == 4) ? GL_RGBA : GL_RGB;
+    glTexImage2D(GL_TEXTURE_2D, 0, static_cast<i32>(format), _resolution.x, _resolution.y, 0, format, GL_UNSIGNED_BYTE, _texture.data());
+    glGenerateMipmap(GL_TEXTURE_2D);
 }
 
 bool RetroFuturaGUI::Texture::IsTextureVerticallyFlipped() const
@@ -37,8 +174,9 @@ std::vector<u8>* RetroFuturaGUI::Texture::GetTextureData()
 
 void RetroFuturaGUI::Texture::loadTexture(std::string_view path)
 {
+    _path = path;
     stbi_set_flip_vertically_on_load(_verticallyFlipped);
-    u8* tempData = stbi_load(path.data(), &_resolution.x, &_resolution.y, &_colorChannelCount, 0);
+    u8* tempData = stbi_load(_path.data(), &_resolution.x, &_resolution.y, &_colorChannelCount, 0);
 
     if (tempData)
         _texture = std::vector<u8>(tempData, tempData + _resolution.x * _resolution.y * _colorChannelCount);
@@ -47,4 +185,24 @@ void RetroFuturaGUI::Texture::loadTexture(std::string_view path)
 
     if(tempData)
         stbi_image_free(tempData);
+}
+
+std::string_view RetroFuturaGUI::Texture::GetPath() const
+{
+    return _path;
+}
+
+void RetroFuturaGUI::Texture::SetType(std::string_view type)
+{
+    _type = type;
+}
+
+std::string_view RetroFuturaGUI::Texture::GetType() const
+{
+    return _type;
+}
+
+u32 RetroFuturaGUI::Texture::GetID() const
+{
+    return _id;
 }
--- source/OpenGlHelpers/Texture.hpp
+++ source/OpenGlHelpers/Texture.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "IncludeHelper.hpp"
+#include "Projection.hpp"
 #include <string>
 #include <string_view>
 #include <vector>
@@ -9,22 +10,66 @@ namespace RetroFuturaGUI
     class Texture
     {
     public:
-        Texture(std::string_view path, const bool flipVertically = true);
+        // projection is only needed if this texture will draw itself as a standalone quad
+        // (e.g. Image2D). Textures used purely as material maps (e.g. Mesh) can omit it
+        Texture(std::string_view path, const bool flipVertically = true, Projection* projection = nullptr);
+        Texture(const Texture&) = delete;
+        Texture& operator=(const Texture&) = delete;
+        Texture(Texture&& other) noexcept;
+        Texture& operator=(Texture&& other) noexcept;
+        ~Texture();
+
+        void Draw() const;
+        void SetSize(const glm::vec2& size);
+        void SetPosition(const glm::vec3& position);
+        void SetRotation(const f32 rotation);
 
         bool IsTextureVerticallyFlipped() const;
         glm::i32vec2 GetResolution() const;
         i32 GetColorChannelCount() const;
         f32 GetAspectRatio() const;
         std::vector<u8>* GetTextureData();
+        std::string_view GetPath() const;
+        void SetType(std::string_view type);
+        std::string_view GetType() const;
+        u32 GetID() const;
 
     private:
         void loadTexture(std::string_view path);
+        void uploadToGPU();
+        void setupQuad();
 
+        u32 _id = 0;
         bool _verticallyFlipped = false;
         f32 _aspectRatio = 0.0f;
-        //std::string _path;
         glm::i32vec2 _resolution = glm::u32vec2(0);
         i32 _colorChannelCount = 0;
         std::vector<u8> _texture;
+        std::string
+            _path {},
+            _type {};
+
+    // Standalone quad rendering (only used when constructed with a Projection)
+        f32 _vertices[(3 + 2) * 4] =
+        {   //   positions     | tex coords
+            -0.5f, -0.5f, 0.0f, 0.0f, 0.0f,
+             0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
+             0.5f,  0.5f, 0.0f, 1.0f, 1.0f,
+            -0.5f,  0.5f, 0.0f, 0.0f, 1.0f
+        };
+        u32 _indices[6] =
+        {
+            0, 1, 2,
+            2, 3, 0
+        };
+        u32
+            _vao { 0 },
+            _vbo { 0 },
+            _ebo { 0 };
+        Projection* _projection { nullptr };
+        glm::mat4
+            _scalingMatrix { 1.0f },
+            _translationMatrix { 1.0f },
+            _rotationMatrix { 1.0f };
     };
 }
