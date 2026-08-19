fc6cc97ea85b11fb6a5f3f81ba9660effed0843a
Lawn Meower <CosmoCortney@users.noreply.github.com>
2026-08-19 22:31:21 +0200

    RadioButtonGroup: Added border features

--- source/Widgets/RadioButtonGroup.cpp
+++ source/Widgets/RadioButtonGroup.cpp
@@ -1,6 +1,61 @@
 #include "RadioButtonGroup.hpp"
+#include "IWidget.hpp"
+#include "IncludeHelper.hpp"
 #include "RadioButton.hpp"
 #include <algorithm>
+#include <memory>
+
+RetroFuturaGUI::RadioButtonGroup::RadioButtonGroup(const std::string& name, Projection* projection, IWidget* parentWidget, const WidgetTypeID parentWidgetTypeID, GLFWwindow* parentWindow)
+    : IWidget(name, projection, parentWidget, parentWidgetTypeID, parentWindow)
+{
+    _widgetTypeID = WidgetTypeID::RadioButtonGroup;
+    _border = std::make_unique<Rectangle>(projection);
+    _text = std::make_unique<Text>(projection);
+
+    if (_border)
+        _border->SetRectangleMode(RectangleMode::Border);
+}
+
+void RetroFuturaGUI::RadioButtonGroup::Draw()
+{
+    drawBorder();
+
+    if(_text)
+        _text->Draw();
+}
+
+void RetroFuturaGUI::RadioButtonGroup::SetSize(const glm::vec3& size)
+{
+    IWidget::SetSize(size);
+
+    if(_border)
+        _border->SetSize(size);
+
+    if(_text)
+        _text->SetParentSize(glm::vec2(size.x, size.y));
+}
+
+void RetroFuturaGUI::RadioButtonGroup::SetPosition(const glm::vec3& position)
+{
+    IWidget::SetPosition(position);
+
+    if(_border)
+        _border->SetPosition(position + glm::vec3(0.0f, 0.0f, 0.01f));
+
+    if(_text)
+        _text->SetPosition(position + glm::vec3(0.0f, 0.0f, 0.02f));
+}
+
+void RetroFuturaGUI::RadioButtonGroup::SetRotation(const glm::vec3& rotation)
+{
+    IWidget::SetRotation(rotation);
+
+    if(_border)
+        _border->SetRotation(rotation);
+
+    if(_text)
+        _text->SetRotation(rotation);
+}
 
 void RetroFuturaGUI::RadioButtonGroup::DeselectOthers(RadioButton* activeRadioButton)
 {
@@ -17,7 +72,7 @@ void RetroFuturaGUI::RadioButtonGroup::RegisterRadioButton(RadioButton* newRadio
 void RetroFuturaGUI::RadioButtonGroup::UnregisterRadioButton(RadioButton* obsoleteRadioButton)
 {
     _radioButtonRefs.erase(
-        std::remove_if(_radioButtonRefs.begin(), _radioButtonRefs.end(), 
+        std::remove_if(_radioButtonRefs.begin(), _radioButtonRefs.end(),
         [&](RadioButton* ptr) { return obsoleteRadioButton == ptr; })
     );
 }
--- source/Widgets/RadioButtonGroup.hpp
+++ source/Widgets/RadioButtonGroup.hpp
@@ -1,19 +1,49 @@
 #pragma once
+#include "ITextProperties.hpp"
 #include "RadioButton.hpp"
+#include "Text.hpp"
+#include <memory>
 
 namespace RetroFuturaGUI
 {
     class RadioButton;
-    
-    class RadioButtonGroup : public IWidget
+
+    class RadioButtonGroup : public IWidget, public IBorder, public ITextProperties
     {
     public:
+        /// @brief Constructs a RadioButtonGroup under the given parent widget/window.
+        RadioButtonGroup(const std::string& name, Projection* projection, IWidget* parentWidget, const WidgetTypeID parentWidgetTypeID, GLFWwindow* parentWindow);
+        RadioButtonGroup(const RadioButtonGroup&) = delete;
+        RadioButtonGroup(RadioButton&&) = delete;
+        ~RadioButtonGroup() = default;
+        auto operator =(const RadioButtonGroup&) = delete;
+        auto operator =(RadioButtonGroup&&) = delete;
 
+        void Draw() override;
+
+        /// @brief Sets the size of the radio button group.
+        void SetSize(const glm::vec3& size) override;
+
+        /// @brief Sets the world position of the radio button group.
+        void SetPosition(const glm::vec3& position) override;
+
+        /// @brief Sets the rotation of the radio button group.
+        void SetRotation(const glm::vec3& rotation) override;
+
+        /// @brief deselcts all registered RadioButtons except the passed one
+        /// @param a pointer of the RadioButton to stay checked
         void DeselectOthers(RadioButton* activeRadioButton);
+
+        /// @brief registers a new RadioButton
+        /// @param a pointer to the new RadioButton
         void RegisterRadioButton(RadioButton* newRadioButton);
+
+        /// @brief unregisters a RadioButton
+        /// @param a pointer to the RadioButton to be unregistered
         void UnregisterRadioButton(RadioButton* obsoleteRadioButton);
 
     private:
         std::vector<RadioButton*> _radioButtonRefs {};
+
     };
 }
